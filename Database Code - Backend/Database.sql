069dedc2d1e174408d84f643954e19662823aa69
CesarB-CS <66040427+CesarB-CS@users.noreply.github.com>
2022-12-06 23:54:16 -0800

    Update Database.sql

--- Database Code - Backend/Database.sql
+++ Database Code - Backend/Database.sql
@@ -11,7 +11,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema FoodOrder
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `FoodOrder` DEFAULT CHARACTER SET utf8 ;
+CREATE DATABASE IF NOT EXISTS `FoodOrder` DEFAULT CHARACTER SET utf8 ;
 USE `FoodOrder` ;
 
 -- -----------------------------------------------------
